0fd296f5f408801b92a73bf67aeb2785e02d259d
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-10 01:11:11 +0000

    Created a test to check if email is well-formed. It fails as intended

---
+++ src/main/java/com/registrationform/registrationform/EmailValidation.java
@@ -0,0 +1,7 @@
+package com.registrationform.registrationform;
+
+public class EmailValidation {
+    public boolean constainsAT(String emailString) {
+        return false;
+    }
+}
---
+++ src/test/java/TestEmail.java
@@ -0,0 +1,11 @@
+import com.registrationform.registrationform.EmailValidation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestEmail {
+    @Test
+    public void TestEmailWellFormed(){
+        EmailValidation newEmail = new EmailValidation();
+        Assertions.assertTrue(newEmail.constainsAT("louis"));
+    }
+}
