6794138c201a7c98e91829717476b73cf82c480f
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-10 01:51:54 +0000

    combined two functions into one validEmail function

--- src/main/java/com/registrationform/registrationform/EmailValidation.java
+++ src/main/java/com/registrationform/registrationform/EmailValidation.java
@@ -2,14 +2,6 @@ package com.registrationform.registrationform;
 
 public class EmailValidation {
 
-    public boolean constainsAT(String emailString) {
-        return emailString.contains("@");
-    }
-
-    public boolean constainsCom(String emailString) {
-        return emailString.contains(".com");
-    }
-
     public boolean validEmail(String emailString) {
         return emailString.contains("@") && emailString.contains(".com");
     }
--- src/test/java/TestEmail.java
+++ src/test/java/TestEmail.java
@@ -6,8 +6,6 @@ public class TestEmail {
     @Test
     public void TestEmailWellFormed(){
         EmailValidation newEmail = new EmailValidation();
-        Assertions.assertTrue(newEmail.constainsAT("louis@"));
-        Assertions.assertTrue(newEmail.constainsCom("louis@gmail.com"));
         Assertions.assertTrue(newEmail.validEmail("louis@gmail.com"));
     }
 }
