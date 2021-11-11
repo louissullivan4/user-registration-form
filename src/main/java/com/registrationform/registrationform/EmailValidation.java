fdf46f9fbafc679814d9330d45debfd5b92d5c70
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-11 22:56:33 +0000

    Realised that you can use regex to match for email. Test fail as the string was not valid

--- src/main/java/com/registrationform/registrationform/EmailValidation.java
+++ src/main/java/com/registrationform/registrationform/EmailValidation.java
@@ -4,14 +4,8 @@ package com.registrationform.registrationform;
 public class EmailValidation {
 
     public boolean validEmail(String emailString) {
-        String atSymbol = "@";
-        char atChar = atSymbol.charAt(0);
-        int atCounter = 0;
-        for (int charcount = 0; charcount < emailString.length(); charcount++){
-            if (emailString.charAt(charcount) == atChar){
-                atCounter = atCounter + 1;
-            }
-        }
-        return atCounter == 1;
+        String emailregex = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        return emailString.matches(emailregex);
     }
 }
--- src/test/java/TestEmail.java
+++ src/test/java/TestEmail.java
@@ -6,7 +6,6 @@ public class TestEmail {
     @Test
     public void TestEmailWellFormed(){
         EmailValidation newEmail = new EmailValidation();
-        //check to make sure string is not empty and is only one value
-        Assertions.assertTrue(newEmail.validEmail("louis123@gmail.com kyle1@gmail.com"));
+        Assertions.assertTrue(newEmail.validEmail("louis123gmail.com"));
     }
 }
