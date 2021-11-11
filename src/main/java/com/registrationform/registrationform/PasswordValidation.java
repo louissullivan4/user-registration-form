acf152af3ee6327155b187084839477215e388fb
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-11 23:36:12 +0000

    checks for password has letter, then digit and then if it has a special character

--- src/main/java/com/registrationform/registrationform/PasswordValidation.java
+++ src/main/java/com/registrationform/registrationform/PasswordValidation.java
@@ -1,7 +1,24 @@
 package com.registrationform.registrationform;
 
 public class PasswordValidation {
-    public boolean validPdLen(String password) {
-        return password.length() >= 7;
+    public boolean validPassword(String password) {
+        String specialCases = "(.*[*^&@!].*$)";
+        boolean passValid;
+        boolean passLetter = false;
+        boolean passDigit= false;
+        for (int i = 0; i < password.length(); i++){
+            char passCharacter = password.charAt(i);
+            if (Character.isLetter(passCharacter)) {
+                passLetter = true;
+            }
+            else if (Character.isDigit(passCharacter)) {
+                passDigit = true;
+            }
+            if (passLetter && passDigit){
+                break;
+            }
+        }
+        passValid = passLetter && passDigit && password.length() >= 7 && password.matches(specialCases);
+        return passValid;
     }
 }
--- src/test/java/TestPassword.java
+++ src/test/java/TestPassword.java
@@ -3,9 +3,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class TestPassword {
+//    @Test
+//    public void TestPasswordLength(){
+//        PasswordValidation newPd = new PasswordValidation();
+//        Assertions.assertTrue(newPd.validPdLen("abc1234"));
+//    }
     @Test
-    public void TestPasswordLength(){
+    public void TestPasswordContainsVals(){
         PasswordValidation newPd = new PasswordValidation();
-        Assertions.assertTrue(newPd.validPdLen("abc1234"));
+        Assertions.assertTrue(newPd.validPassword("*@abc2345"));
     }
 }
