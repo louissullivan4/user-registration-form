86af6f98d2b4d8cfd9aa9487b1375bbe23a4923a
louissullivan4 <sullivanlouis0@gmail.com>
2021-11-11 22:57:31 +0000

    File now runs correctly

--- src/test/java/TestEmail.java
+++ src/test/java/TestEmail.java
@@ -6,6 +6,6 @@ public class TestEmail {
     @Test
     public void TestEmailWellFormed(){
         EmailValidation newEmail = new EmailValidation();
-        Assertions.assertTrue(newEmail.validEmail("louis123gmail.com"));
+        Assertions.assertTrue(newEmail.validEmail("louis123@gmail.com"));
     }
 }
